1f83eecf45ce4746e8c2658e1a348c90c113f693
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-17 20:25:19 -0300

    Add: Challenge 2 - Its a prime Number?

--- Day_Eight.py
+++ Day_Eight.py
@@ -35,3 +35,17 @@ test_h = int(input()) # Height of wall (m)
 test_w = int(input()) # Width of wall (m)
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
+#Challenge 2 - Its a Prime Number?
+
+def prime_checker(number):
+  in_prime = True
+  for i in range(2, number):
+    if number % i == 0:
+      in_prime = False
+
+
+  if in_prime == True:
+    print("It's a prime number.")
+  if in_prime == False:
+    print("It's not a prime number.")
