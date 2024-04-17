42efe11348ae9084bdd7efa15b93ff103e357faf
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-17 13:10:40 -0300

    Add: Challenge 3 - Checking if the Player has Won

--- Day_Seven.py
+++ Day_Seven.py
@@ -4,25 +4,32 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 list_random = random.choice(word_list)
-
-Letter = str(input("Digite anything letter ")).lower()
-print(list_random)
-
+word_lenght = len(list_random)
 
 display = []
 for _ in range(len(list_random)):
-    display += "_"
+        display += "_"
 
-for position in range(len(list_random)):
-    letters = list_random[position]
-    if Letter == letters:
-        print("yes")      
-        display[position] = Letter  
-    else:
-        print("No")
+end_of_game = False
 
-print(display)
+while not end_of_game:
+
+    Letter = (input("Digite anything letter ")).lower()
+
+    for position in range(0, word_lenght):
+        letters = list_random[position]
+
+        if letters == Letter:  
+            display[position] = Letter  
+            
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
 
 #Challenge 2 - Replacing Blanks with Guesses
+
+#Challenge 3 = Checking if the Player has Won
 
 
