f1e58183fc1c60e80ca41884caae3ccd650be7c9
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-26 13:21:51 -0300

    Change the Code

--- Day_Twelve.py
+++ Day_Twelve.py
@@ -1,54 +1,42 @@
 #Namespaces: Local vs. Global Scope
-import random
+from random import randint
 
  
-should_continue = True
-attempts = 0
-ansyer = []
-for i in range(1, 101):
-    ansyer.append(i)
+EASY_LEVEL = 10
+HARD_LEVEL = 5
 
-thinking_number = random.choice(ansyer)
-print(thinking_number)
+def difficulty_return(guess , answer):
+    if guess > ansyer:
+        print("Too High")
+    elif guess < ansyer:
+        print("Too Low")
+    else:
+        print(f"You got it! The  answer is {ansyer}")
+    
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    
+    if difficulty == "easy":
+       return turns = EASY_LEVEL
+    else:
+       return turns = HARD_LEVEL
+
+
 
 print("Welcome to the Nubmer Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+ansyer = randint(1, 100)
 
-def difficulty_choice():
-    global attempts
-    if difficulty == "easy":
-        attempts = 10
-    elif difficulty == "hard":
-        attempts = 5    
-    return attempts   
-attempts_choice = difficulty_choice()
 
-def difficulty_return(guess_choice):
-    if guess_choice == thinking_number:
-        print("You Guess Right!!!")
-        should_continue = False
-        return should_continue
-    elif guess_choice > thinking_number:
-        print("Too High")
-        print("Guess again")
-        attempts_choice - 1
-    else:
-        print("Too low")
-        print("Guess again")
-        attempts_choice - 1
+guess = int(input("Make a Guess"))
+turns = set_difficulty()
+print(f"You Have {turns} attmpts remaining")
+
+
+
+
     
-  
 
-
-
-
-
-while should_continue:
-    print(f"You have {attempts_choice} attempts remaining to guesse the number")
-    guess = int(input("Make a guess: "))
-
-    difficulty_return(guess)
 
 
 
