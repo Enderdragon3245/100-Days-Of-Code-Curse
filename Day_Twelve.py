4ec964ffb70f005c69820ee7875bd525281e1421
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-26 13:46:21 -0300

    Finishing the Guess Number Challenge

--- Day_Twelve.py
+++ Day_Twelve.py
@@ -5,38 +5,42 @@ from random import randint
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 
-def difficulty_return(guess , answer):
-    if guess > ansyer:
+
+def check_answer(guess , answer, turns):
+    """ Pass a guesse number who comaparet to the answer, return a turns - 1 or anything"""
+    if guess > answer:
         print("Too High")
-    elif guess < ansyer:
+        return turns - 1
+    elif guess < answer:
         print("Too Low")
+        return turns - 1
     else:
-        print(f"You got it! The  answer is {ansyer}")
+        print(f"You got it! The  answer is {answer}")
     
 def set_difficulty():
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
     
     if difficulty == "easy":
-       return turns = EASY_LEVEL
+       return EASY_LEVEL
     else:
-       return turns = HARD_LEVEL
+       return HARD_LEVEL
 
 
+def game():
+    print("Welcome to the Nubmer Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    answer = randint(1, 100)
 
-print("Welcome to the Nubmer Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
-ansyer = randint(1, 100)
-
-
-guess = int(input("Make a Guess"))
-turns = set_difficulty()
-print(f"You Have {turns} attmpts remaining")
-
-
-
-
+    turns = set_difficulty()
     
 
+    guess = 0
+    while guess != answer:
+        print(f"You Have {turns} attmpts remaining")
+        guess = int(input("Make a Guess: "))
 
-
-
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose")
+            break
+game()
