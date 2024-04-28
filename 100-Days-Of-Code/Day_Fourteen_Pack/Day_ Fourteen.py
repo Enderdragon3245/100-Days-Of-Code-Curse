23b64576ccd7685b2fd7d9e1edbc5182fd7edb47
Gustavo.M <Enderdragon3245@gmail.com>
2024-04-28 15:08:30 -0300

    Add: Def Remove_equals

--- 100-Days-Of-Code/Day_Fourteen_Pack/Day_ Fourteen.py
+++ 100-Days-Of-Code/Day_Fourteen_Pack/Day_ Fourteen.py
@@ -13,7 +13,22 @@ def Compare_A(Number):
     Compare_follower_count = data[Number]["follower_count"]
 
     print(f"Compare A: {Compare_name}, {Compare_Description}, from {Compare_County}")
-    
 
-Compareted_Number = randint(0, 15)
-Compare_A(Compareted_Number)
+#Remove equals Compareteds in Game.
+def Remove_equals(Number, Number2):
+    global Compareted_Number_B
+
+    while Number == Number2:
+        Compareted_Change = randint(0, 15)
+        Compareted_Number_B = Compareted_Change
+        return Compareted_Number_B
+        
+
+
+Compareted_Number_A = randint(0, 5)
+Compareted_Number_B = randint(0, 5)
+
+Compare_A(Compareted_Number_A)
+Compare_A(Compareted_Number_B)
+Remove_equals(Compareted_Number_A, Compareted_Number_B)
+print(Compareted_Number_B)
