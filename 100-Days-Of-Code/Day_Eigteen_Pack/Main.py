6f21134a698e17c9716028aea2a797aff628d9ba
Gustavo.M <Enderdragon3245@gmail.com>
2024-05-07 19:40:00 -0300

    Add New Turtle

--- 100-Days-Of-Code/Day_Eigteen_Pack/Main.py
+++ 100-Days-Of-Code/Day_Eigteen_Pack/Main.py
@@ -1,10 +1,14 @@
 from turtle import *
 
 t = Turtle()
+t2 = Turtle()
+t2.color("red")
 
 for ang in range(0, 4):
     t.forward(100)
     t.left(90)
+    t2.left(90)
+    t2.forward(100)
 
 
 screen = Screen()
