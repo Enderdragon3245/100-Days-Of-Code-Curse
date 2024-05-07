08abc61660f43da05082061955c779b9ed1d7e87
Gustavo.M <Enderdragon3245@gmail.com>
2024-05-07 20:05:03 -0300

    Challenge 3: Draw Many Shapes

--- 100-Days-Of-Code/Day_Eigteen_Pack/Main.py
+++ 100-Days-Of-Code/Day_Eigteen_Pack/Main.py
@@ -12,12 +12,27 @@ for ang in range(0, 4):
     t2.left(90)
     t2.forward(100)
 
-"""
 for ang in range(0 , 10):
     t.pendown()
     t.forward(10)
     t.penup()
     t.forward(10)
+
+"""
+sides = 3
+angle = 360
+is_on = True
+
+
+while is_on:
+    for ang in range(0 , sides):
+        result = angle / sides
+        t.forward(100)
+        t.right(result)
+    
+    sides += 1
+
+    
 
 screen = Screen()
 screen.exitonclick()
